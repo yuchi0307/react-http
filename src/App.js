156be1848bb38c943c570b42598f66c9f921b0cf
yuchi0307 <gelee0307@gmail.com>
2021-12-07 21:02:06 +0800

    change firebase url

--- src/App.js
+++ src/App.js
@@ -6,27 +6,29 @@ import useHttp from './components/hooks/use-http';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  console.log("server:",tasks);
  //將傳入costum hook的第二個參數作為一個變數如下行：
-  const transformTask = taskObj => { //從firebase拿來的資料，轉為前端需要的樣子
+  const transformTask = tasksObj => { //從firebase拿來的資料，轉為前端需要的樣子
     const loadedTasks = [];
 
-      for (const taskKey in taskObj) {
-        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-      }
-      setTasks(loadedTasks);
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
 
+    setTasks(loadedTasks);
   }
   //useHttp回傳的物件放在這，將sendRequest轉名為fetchTasks
   const {isLoading, error, sendRequest: fetchTasks} = useHttp(
-    { url: 'https://tasks-44545-default-rtdb.firebaseio.com/tasks.json' },
+    { url: 'https://tasks-7d7de-default-rtdb.firebaseio.com/tasks.json' },
     transformTask
   );
 
 
   useEffect(() => {
     fetchTasks();
+    console.log('fetchtask', fetchTasks)
     // eslint-disable-next-line
-  }, []);
+  },[]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://tasks-44545-default-rtdb.firebaseio.com/tasks.json',
+        'https://tasks-7d7de-default-rtdb.firebaseio.com/tasks.json',
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
